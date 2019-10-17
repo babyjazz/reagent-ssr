6e85428f67a9b07928a1db0eae113efe7ed704a4
Jitiwattana <bbos1@windowslive.com>
2019-10-18 00:27:28 +0700

    fix prod wrong ns

--- env/prod/cljs/reagnet_serverside/prod.cljs
+++ env/prod/cljs/reagnet_serverside/prod.cljs
@@ -1,5 +1,5 @@
-(ns reagnet-serverside.prod
-  (:require [reagnet-serverside.core :as core]))
+(ns reagent-serverside.prod
+  (:require [reagent-serverside.core :as core]))
 
 ;;ignore println statements in prod
 (set! *print-fn* (fn [& _]))
