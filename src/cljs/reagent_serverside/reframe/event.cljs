30cc7b09a480fcac3c220e327a05c36b04bb82f9
Jitiwattana <bbos1@windowslive.com>
2019-11-02 20:53:20 +0700

    remove re-frame trace

--- src/cljs/reagent_serverside/reframe/event.cljs
+++ src/cljs/reagent_serverside/reframe/event.cljs
@@ -1,17 +1,16 @@
 (ns reagent-serverside.reframe.event
   (:require
    [re-frame.core :as rf]
-   [day8.re-frame.tracing :refer-macros [fn-traced]]
    [reagent-serverside.router.router :refer [initial-page]]))
 
 ; dispatch events
 ; -------------------------------
 (rf/reg-event-db
  :set-api
- (fn-traced [db [_ value]]
+ (fn [db [_ value]]
             (assoc db :api (into (get db :api) value))))
 
 (rf/reg-event-db
  :set-router
- (fn-traced [db [_ new-page]]
+ (fn [db [_ new-page]]
             (assoc db :router (into (get db :router) (into initial-page new-page)))))
